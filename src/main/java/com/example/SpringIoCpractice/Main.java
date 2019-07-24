c974f51de4f2e02a090a11e1b0392d39853307da
yangjingsheng <673630886@qq.com>
2019-07-24 20:01:22 +0800

    add

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,17 @@
-//TODO
+package com.example.SpringIoCpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorld.class, args);
+    }
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("hello.xml");
+    // 2.从容器中获取Bean实例
+    HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
+}
