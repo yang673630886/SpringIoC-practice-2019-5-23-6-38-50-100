4bfd14c906437c71a6e775016072c2b4a16d74df
yangjingsheng <673630886@qq.com>
2019-07-25 17:01:17 +0800

    修改内容

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -4,14 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorld.class, args);
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("hello.xml");
+        // 2.从容器中获取Bean实例
+        HelloWorld hw = (HelloWorld) ctx.getBean("helloworld");
+        hw.getHello();
+
     }
-    ApplicationContext ctx = new ClassPathXmlApplicationContext("hello.xml");
-    // 2.从容器中获取Bean实例
-    HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
+
+
+
+
+
+
+
+
 }
--- src/test/java/com/example/SpringIoCpractice/SpringIoCPracticeApplicationTests.java
+++ src/test/java/com/example/SpringIoCpractice/SpringIoCPracticeApplicationTests.java
@@ -3,6 +3,9 @@ package com.example.SpringIoCpractice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +14,10 @@ public class SpringIoCPracticeApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		ApplicationContext ctxx = new FileSystemXmlApplicationContext("hello.xml");
+		// 2.从容器中获取Bean实例
+		HelloWorld ho = (HelloWorld) ctxx.getBean("helloworld");
+		ho.getHello();
 	}
 
 }
